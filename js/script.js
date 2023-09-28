dbd21ee4e88afde2134be3b23f6b2136825be571
Fahru Rojak <gfahru5@gmail.com>
2023-09-28 18:58:17 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let menubar = document.querySelector('#menubar');
+let mynav = document.querySelector('.navbar');
+
+menubar.onclick = () => {
+    menubar.classList.toogle('fa-times')
+}
+
+// Fungsi untuk menampilkan pop-up
+function openform() {
+    document.getElementById("bukaform").style.display = "block";
+}
+
+// Fungsi untuk menutup pop-up
+function tutupform() {
+    document.getElementById("bukaform").style.display = "none";
+}
+
+
